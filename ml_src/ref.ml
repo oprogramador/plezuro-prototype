e3015e865cf2d46c94c4ba784fb8562e55abc780
pierre <uomodislesia@gmail.com>
2014-09-22 00:55:11 +0200

    new ml files

---
+++ ml_src/ref.ml
@@ -0,0 +1,19 @@
+$a = 21;
+$b = a;
+b++;
+b.::printl(); //it prints '22'
+(a===b).::printl(); //it prints 'true'
+
+$c = 4;
+$d := c;
+d++;
+c.::printl(); //it prints '4'
+(c===d).::printl(); //it prints 'false'
+
+$e = 2;
+$f = e.::clone();
+f++;
+e.::printl(); //it prints '2'
+(e===f).::printl(); //it prints 'false'
+
+null
