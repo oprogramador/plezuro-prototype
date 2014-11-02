686ff51b3d9400c5275de0b006d924296b040ce2
pierre <uomodislesia@gmail.com>
2014-11-02 02:24:22 +0100

    labirynth optimization

--- src/ml_src/labir.ml
+++ src/ml_src/labir.ml
@@ -3,9 +3,9 @@ $maze = {
         $n := x*y-1; 
         $horiz;
         $verti;
-        {n<0}.if({
+        {n<0}.if{
                 "illegal maze dimensions".printl
-        }).else({
+        }.else{
                 $max = [x,y].max+1;
                 horiz := [[false]*max]*(x+1);
                 verti := [[false]*max]*(y+1); 
@@ -18,7 +18,7 @@ $maze = {
                                 unvisited[j] << (j>0 & j<x+1 & k>0 & (j!=(here[0]+1) | k!=(here[1]+1)))
                         });
                 });
-                {0<n}.while({
+                {0<n}.while{
                         $potential := [[here[0]+1, here[1]], [here[0],here[1]+1],
                                 [here[0]-1, here[1]], [here[0],here[1]-1]];
                         $neighbors := [];
@@ -27,30 +27,21 @@ $maze = {
                                         neighbors << potential[j]
                                 })
                         });
-                        {neighbors.len>0}.if({
+                        {neighbors.len>0}.if{
                                 n--;
                                 $next := neighbors[(rand*neighbors.len).floor];
-                                $row = unvisited[next[0]+1];
-                                row = [row[0..(next[1]+1)]] + [false] + [row[(next[1]+2)..(row.len)]];
-                                unvisited[next[0]+1] = row;
-                                //unvisited[next[0]+1][next[1]+1] := false;
-                                {next[0] == here[0]}.if({
-                                        $row = horiz[next[0]];
-                                        row = [row[0..((next[1]+here[1]-1)/2)]] + [true] + [row[(((next[1]+here[1]-1)/2)+1)..(row.len)]];
-                                        horiz[next[0]] = row;
-                                        //horiz[next[0]][(next[1]+here[1]-1)/2] := true
-                                }).else({
-                                        $row = verti[(next[0]+here[0]-1)/2];
-                                        row = [row[0..(next[1])]] + [true] + [row[(next[1]+1)..(row.len)]];
-                                        verti[(next[0]+here[0]-1)/2] = row;
-                                        //verti[(next[0]+here[0]-1)/2][next[1]] := true
-                                });
+                                unvisited[next[0]+1][next[1]+1] := false;
+                                {next[0] == here[0]}.if{
+                                        horiz[next[0]][(next[1]+here[1]-1)/2] := true
+                                }.else{
+                                        verti[(next[0]+here[0]-1)/2][next[1]] := true
+                                };
                                 path << (here := next);
-                        }).else({
+                        }.else{
                                 path >> here;
-                        });
-                })
-        });
+                        };
+                }
+        };
         #['x',x, 'y',y, 'horiz',horiz, 'verti',verti]
 };
 
@@ -59,37 +50,37 @@ $display = {
         $text = '';
         0..(m['x']*2+1).eachi($j,{
                 $line = [];
-                {0==j%2}.if({
+                {0==j%2}.if{
                         0..(m['y']*4+1).eachi($k,{
-                                {0==k%4}.if({
+                                {0==k%4}.if{
                                         line << '*'
-                                }).else({
-                                        {j>0 & m['verti'][j/2-1][(k/4).floor]}.if({
+                                }.else{
+                                        {j>0 & m['verti'][j/2-1][(k/4).floor]}.if{
                                                 line << ' '
-                                        }).else({
+                                        }.else{
                                                 line << '-'
-                                        })
-                                })
+                                        }
+                                }
                         })
-                }).else({
+                }.else{
                         0..(m['y']*4+1).eachi($k,{
-                                {0==k%4}.if({
-                                        {k>0 & m['horiz'][(j-1)/2][k/4-1]}.if({
+                                {0==k%4}.if{
+                                        {k>0 & m['horiz'][(j-1)/2][k/4-1]}.if{
                                                 line << ' '
-                                        }).else({
+                                        }.else({
                                                 line << '|'
-                                        })
-                                }).else({
+                                        }
+                                }.else{
                                         line << ' '
-                                })
-                        })
-                });
-                {0==j}.if({
+                                }
+                        }
+                };
+                {0==j}.if{
                         line[1] = line[3] = line[2] = ' '
-                });
-                {m['x']*2-1==j}.if({
+                };
+                {m['x']*2-1==j}.if{
                         line[4*m['y']] = ' '
-                });
+                };
                 text += (line.toTup.toS+'\n')
         });
         text
