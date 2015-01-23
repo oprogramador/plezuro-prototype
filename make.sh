0bf237f61029f070bd1b22bba21bac98b2169be8
oprogramador <uomodislesia@gmail.com>
2015-01-23 10:27:50 +0100

    ide-compiler split

--- make.sh
+++ make.sh
@@ -1,7 +1,10 @@
-#mkdir -p ../res
-#if [ ! -f ../res/world.db ]; then
-#	cp world.db ../res/world.db
-#fi
-gmcs -pkg:dotnet -out:plezuro.exe `find src -name '*.cs'` &&
-chmod 775 plezuro.exe &&
-sudo cp plezuro.exe /usr/bin/plezuro
+if [ $1 = 'c' -o $1 = '' ]; then
+    gmcs -pkg:dotnet -out:plezuro.exe `find src/csharp_src/Mondo/Compiler -name '*.cs'` &&
+    chmod 775 plezuro.exe &&
+    sudo cp plezuro.exe /usr/bin/plezuro
+fi
+if [ $1 = 'i' -o $1 = '' ]; then
+    gmcs -pkg:dotnet -out:plezuro_studio.exe `find src/csharp_src/Mondo/IDE -name '*.cs'` &&
+    chmod 775 plezuro_studio.exe &&
+    sudo cp plezuro_studio.exe /usr/bin/plezuro_studio
+fi
