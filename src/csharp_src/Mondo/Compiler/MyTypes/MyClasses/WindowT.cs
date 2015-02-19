4894041e9f57018f3904e850dcc3f30df3e81086
oprogramador <uomodislesia@gmail.com>
2015-02-19 01:22:09 +0100

    double buffering

--- src/csharp_src/Mondo/Compiler/MyTypes/MyClasses/WindowT.cs
+++ src/csharp_src/Mondo/Compiler/MyTypes/MyClasses/WindowT.cs
@@ -24,121 +24,136 @@
 using System;
 using System.Windows.Forms;
 using System.Drawing;
+using System.Timers;
 using System.Collections.Generic;
 using Mondo.Engine;
 using Mondo.MyCollections;
 
 namespace Mondo.MyTypes.MyClasses {
-	class WindowT : Form, IVariable {
-		public int ID { get; private set; }
-                public DictionaryT Dictionary { get; private set; }
-                public ProcedureT OnKeyPressProc { get; private set; }
-                private IPrintable printable;
-                private ListT drawingSquares {
-                    get {
-                       return (ListT)((PointerT)((ReferenceT)Dictionary[new ReferenceT(new StringT("squares"))]).Value).Value.Value;
-                    }
-                    set {
-                        //Console.WriteLine("set");
-                        //Dictionary[new ReferenceT(new StringT("squares"))] = new ReferenceT(value); 
-                    }
+    class WindowT : Form, IVariable {
+        private System.Timers.Timer timer;
+        public int ID { get; private set; }
+        public DictionaryT Dictionary { get; private set; }
+        public ProcedureT OnKeyPressProc { get; private set; }
+        private IPrintable printable;
+        private ListT drawingSquares {
+            get {
+                return (ListT)((PointerT)((ReferenceT)Dictionary[new ReferenceT(new StringT("squares"))]).Value).Value.Value;
+            }
+            set {
+                //Console.WriteLine("set");
+                //Dictionary[new ReferenceT(new StringT("squares"))] = new ReferenceT(value); 
+            }
+        }
+        private int drawingSquaresWidth {
+            get {
+                return ((StringT)((ReferenceT)drawingSquares[0]).Value).Value.Length;
+            }
+            set {}
+        }
+        public Dictionary<char,Color> Colors = new Dictionary<char,Color>() {
+            {'r', Color.Red},
+                {'g', Color.Green},
+                {'b', Color.Blue},
+                {'y', Color.Yellow},
+        };
+
+        public WindowT(IPrintable p, DictionaryT dic) {
+            ID = ObjectContainer.Instance.Add(this);
+            DoubleBuffered = true;
+            printable = p;
+            Width = (int)((Number)((ReferenceT)dic[new ReferenceT(new StringT("w"))]).Value).Value;
+            Height = (int)((Number)((ReferenceT)dic[new ReferenceT(new StringT("h"))]).Value).Value;
+            try {
+                int tt = (int)((Number)((ReferenceT)dic[new ReferenceT(new StringT("time"))]).Value).Value;
+                timer = new System.Timers.Timer(tt);
+                timer.Elapsed += onTimeEvent;
+                timer.Enabled = true;
+            } catch {}
+            OnKeyPressProc = (ProcedureT)((ReferenceT)dic[new ReferenceT(new StringT("keypress"))]).Value;
+            Dictionary = dic;
+            KeyPress += new KeyPressEventHandler(onKeyPress);
+        }
+
+        private void onKeyPress(object sender, KeyPressEventArgs e) {
+            OnKeyPressProc.Call(printable, new object[]{new StringT(""+e.KeyChar)});
+        }
+
+        private void onTimeEvent(Object source, ElapsedEventArgs e) {
+            Refresh();
+        }
+
+        public override bool Equals(object ob) {
+            return CompareTo(ob)==0;
+        }
+
+        public override int GetHashCode() {
+            return base.GetHashCode();
+        }
+
+        public int CompareTo(object ob) {
+            int pre = ReferenceT.PreCompare(this,ob);
+            if(pre!=0) return pre;
+            return 0;
+        }
+
+        public object Clone() {
+            return new WindowT(printable, (DictionaryT)Dictionary.Clone());
+        }
+
+        IVariable[] ITuplable.ToArray() {
+            return new IVariable[]{};
+        }
+
+        private static ClassT myClass;
+
+        public const string ClassName = "Window";
+
+        private static object[] lambdas = {
+            "show", 	(Func<WindowT,bool>) ((x) => {x.Show(); Application.Run(x); return true;}),
+            "close",	(Func<WindowT,bool>) ((x) => {x.Close(); return true;}),
+        };
+
+        public ClassT GetClass() {
+            return StaticGetClass();
+        }
+
+        public static ClassT StaticGetClass() {
+            if(myClass==null) myClass = 
+                new BuiltinClass( ClassName, new List<ClassT>(){ObjectT.StaticGetClass()}, LambdaConverter.Convert(lambdas), PackageT.Lang, typeof(WindowT) );
+            return myClass;
+        }	
+
+        public object ObValue {
+            get { return this; }
+            set { }
+        }
+
+        public override string ToString() {
+            return "window( "+Dictionary+" )";
+        }
+
+        protected override void OnPaint(PaintEventArgs e) {
+            base.OnPaint(e);
+            var g = e.Graphics;
+            if(drawingSquares!=null) drawSquares(g);
+            Console.WriteLine("ds="+drawingSquares);
+            Console.WriteLine("this="+this);
+            g.Dispose();
+        }
+
+        private void drawSquares(Graphics g) {
+            Console.WriteLine("ds");
+            int ww = Width/drawingSquaresWidth;
+            int hh = Height/drawingSquares.Count;
+            for(int y=0; y<drawingSquares.Count; y++) {
+                var str = ((StringT)((ReferenceT)drawingSquares[y]).Value).Value;
+                for(int x=0; x<str.Length; x++) {
+                    var brush = new SolidBrush(Colors[str[x]]);
+                    g.FillRectangle(brush, new Rectangle(x*ww, y*hh, ww, hh));
+                    brush.Dispose();
                 }
-                private int drawingSquaresWidth {
-                    get {
-                        return ((StringT)((ReferenceT)drawingSquares[0]).Value).Value.Length;
-                    }
-                    set {}
-                }
-                public Dictionary<char,Color> Colors = new Dictionary<char,Color>() {
-                    {'r', Color.Red},
-                    {'g', Color.Green},
-                    {'b', Color.Blue},
-                    {'y', Color.Yellow},
-                };
-
-		public WindowT(IPrintable p, DictionaryT dic) {
-			ID = ObjectContainer.Instance.Add(this);
-                        printable = p;
-                        Width = (int)((Number)((ReferenceT)dic[new ReferenceT(new StringT("w"))]).Value).Value;
-                        Height = (int)((Number)((ReferenceT)dic[new ReferenceT(new StringT("h"))]).Value).Value;
-                        OnKeyPressProc = (ProcedureT)((ReferenceT)dic[new ReferenceT(new StringT("keypress"))]).Value;
-                        Dictionary = dic;
-			KeyPress += new KeyPressEventHandler(onKeyPress);
-		}
-
-		private void onKeyPress(object sender, KeyPressEventArgs e) {
-                    OnKeyPressProc.Call(printable, new object[]{new StringT(""+e.KeyChar)});
-		}
-		public override bool Equals(object ob) {
-			return CompareTo(ob)==0;
-		}
-
-		public override int GetHashCode() {
-			return base.GetHashCode();
-		}
-
-		public int CompareTo(object ob) {
-			int pre = ReferenceT.PreCompare(this,ob);
-			if(pre!=0) return pre;
-			return 0;
-		}
-
-		public object Clone() {
-			return new WindowT(printable, (DictionaryT)Dictionary.Clone());
-		}
-
-		IVariable[] ITuplable.ToArray() {
-			return new IVariable[]{};
-		}
-
-		private static ClassT myClass;
-
-		public const string ClassName = "Window";
-
-		private static object[] lambdas = {
-                        "show", 	(Func<WindowT,bool>) ((x) => {x.Show(); Application.Run(x); return true;}),
-                        "close",	(Func<WindowT,bool>) ((x) => {x.Close(); return true;}),
-		};
-
-		public ClassT GetClass() {
-			return StaticGetClass();
-		}
-
-		public static ClassT StaticGetClass() {
-			if(myClass==null) myClass = 
-				new BuiltinClass( ClassName, new List<ClassT>(){ObjectT.StaticGetClass()}, LambdaConverter.Convert(lambdas), PackageT.Lang, typeof(WindowT) );
-			return myClass;
-		}	
-
-		public object ObValue {
-			get { return this; }
-			set { }
-		}
-
-		public override string ToString() {
-			return "window( "+Dictionary+" )";
-		}
-
-                protected override void OnPaint(PaintEventArgs e) {
-                    var g = this.CreateGraphics();
-                    if(drawingSquares!=null) drawSquares(g);
-                    Console.WriteLine("ds="+drawingSquares);
-                    Console.WriteLine("this="+this);
-                    g.Dispose();
-                }
-
-                private void drawSquares(Graphics g) {
-                    Console.WriteLine("ds");
-                    int ww = Width/drawingSquaresWidth;
-                    int hh = Height/drawingSquares.Count;
-                    for(int y=0; y<drawingSquares.Count; y++) {
-                        var str = ((StringT)((ReferenceT)drawingSquares[y]).Value).Value;
-                        for(int x=0; x<str.Length; x++) {
-                            var brush = new SolidBrush(Colors[str[x]]);
-                            g.FillRectangle(brush, new Rectangle(x*ww, y*hh, ww, hh));
-                            brush.Dispose();
-                        }
-                    }
-                }
-	}
+            }
+        }
+    }
 }
--- src/ml_src/window.ml
+++ src/ml_src/window.ml
@@ -3,6 +3,7 @@ $squares = ['gg','rb'];
 $w = #[
     'w', 830,
     'h', 470,
+    'time', 50,
     'keypress', {squares=['rgb', 'ggb']},
     'squares', &&squares
     ].window;
