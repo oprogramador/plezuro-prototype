c83b7563da78e61cd8171badfa9f712d0a5f7b98
oprogramador <uomodislesia@gmail.com>
2015-02-20 07:02:30 +0100

    snike algorithm

--- src/ml_src/snike.ml
+++ src/ml_src/snike.ml
@@ -16,6 +16,25 @@ $printOnBoard = {
     };
     board
 };
+$addVec = {
+    [this[0]+first[0], this[1]+first[1]]
+};
+$moveSnike = {
+    $directions = #[
+        'w', [0, -1],
+        'a', [-1, 0],
+        's', [0, 1],
+        'd', [1, 0]
+    ];
+    $new = [];
+    new << addVec(this[0]);
+    this.each{
+        {first>0}.if{
+            new << this
+        }
+    };
+    new
+};
 $squares = makeBoard('y',20,6);
 printOnBoard($squares, [1,25,2]);
 $squares = printOnBoard(squares, [#['color', 'r', 'sq', [[0,1], [2,4]]], #['color', 'b', 'sq', [[2,5], [15,3]]]]);
